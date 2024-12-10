c875f8f99ce3acf8cf296feee54eb692799bbd6b
JungerBoyo <pZ010001011111@proton.me>
2024-12-10 10:04:05 +0000

    Improve CLI interface and fix too lines

--- main.py
+++ main.py
@@ -7,15 +7,39 @@ from transformers import PegasusTokenizer, PegasusForConditionalGeneration
 from rouge_score import rouge_scorer
 from bert_score import score as bert_score
 
-EXTRACTION_BASED_SUMMARY_NAIVE_SCORING = 0x01
-EXTRACTION_BASED_SUMMARY_FIRST_LAST_SCORING = 0x02
-EXTRACTION_BASED_SUMMARY_TF_IDF_SCORING = 0x04
-EXTRACTION_BASED_SUMMARY_TEXT_RANK_SCORING = 0x08
-EXTRACTION_BASED_SUMMARY = 0x0F
+EXT_NAIVE_SCORING = 0x01
+EXT_FIRST_LAST_SCORING = 0x02
+EXT_TF_IDF_SCORING = 0x04
+EXT_TEXT_RANK_SCORING = 0x08
+EXT_SUMMARY = 0x0F
 
-ABSTRACT_SUMMARY_T5 = 0x10
-ABSTRACT_SUMMARY_PEGASUS = 0x20
-ABSTRACT_SUMMARY = 0xF0
+ABST_T5 = 0x10
+ABST_PEGASUS = 0x20
+ABST_SUMMARY = 0xF0
+
+METHODS_MAP = dict([
+    ('EXT_NAIVE', EXT_NAIVE_SCORING),
+    ('EXT_FIRST_LAST', EXT_FIRST_LAST_SCORING),
+    ('EXT_IF_IDF', EXT_TF_IDF_SCORING),
+    ('EXT_TEXT_RANK', EXT_TEXT_RANK_SCORING),
+    ('ABST_T5', ABS_T5),
+    ('ABST_PEGASUS', ABS_PEGASUS)
+])
+
+
+def is_method_set(method, bit):
+    return (method & bit) > 0
+
+
+def parse_method(method_str):
+    methods = method_str.split('|')
+    result = 0
+    for method_str in methods:
+        method = METHODS_MAP.get(method_str)
+        if method is None:
+            return 0
+        result |= method
+    return result
 
 
 def extraction_based_summarize_naive_scoring(doc):
@@ -41,7 +65,8 @@ def extraction_based_summarize_tfidf_sentence_scoring(doc):
 
     sent_score_map = {}
     for sent_id in sorted_indices[0, :].tolist()[0]:
-        sent_score_map[sent_id] = (sentence_scores[sent_id] - min_score) / (max_score - min_score)
+        sent_score_map[sent_id] = (sentence_scores[sent_id] - min_score) / \
+            (max_score - min_score)
 
     return sent_score_map
 
@@ -50,7 +75,8 @@ def extraction_based_summarize_text_rank_sentence_scoring(doc):
     doc_sents_list = list(doc.sents)
     sentence_scores = {}
 
-    for j, sent_tr in enumerate(doc._.textrank.summary(limit_sentences=len(doc_sents_list))):
+    for j, sent_tr in enumerate(doc._.textrank.summary(
+            limit_sentences=len(doc_sents_list))):
         sent_id = 0
         for i, sent in enumerate(doc_sents_list):
             if sent.text == sent_tr.text:
@@ -71,10 +97,10 @@ def add_base(sents_map, base):
 def extraction_based_summarize(doc, method, num_sentences):
     doc_sents_list = list(doc.sents)
 
-    use_naive_scoring = bool((method & EXTRACTION_BASED_SUMMARY_NAIVE_SCORING) > 0)
-    use_first_last_scoring = bool((method & EXTRACTION_BASED_SUMMARY_FIRST_LAST_SCORING) > 0)
-    use_tf_idf_scoring = bool((method & EXTRACTION_BASED_SUMMARY_TF_IDF_SCORING) > 0)
-    use_text_rank_scoring = bool((method & EXTRACTION_BASED_SUMMARY_TEXT_RANK_SCORING) > 0)
+    use_naive_scoring = is_method_set(method, EXT_NAIVE_SCORING)
+    use_first_last_scoring = is_method_set(method, EXT_FIRST_LAST_SCORING)
+    use_tf_idf_scoring = is_method_set(method, EXT_TF_IDF_SCORING)
+    use_text_rank_scoring = is_method_set(method, EXT_TEXT_RANK_SCORING)
 
     first_sent = doc_sents_list[0]
     last_sent = doc_sents_list[-1]
@@ -93,11 +119,13 @@ def extraction_based_summarize(doc, method, num_sentences):
         sent_id_score_maps.append(add_base(naive_sents_map, 0.2))
 
     if use_tf_idf_scoring:
-        tfidf_sents_map = extraction_based_summarize_tfidf_sentence_scoring(doc)
+        tfidf_sents_map = \
+                extraction_based_summarize_tfidf_sentence_scoring(doc)
         sent_id_score_maps.append(add_base(tfidf_sents_map, 0.4))
 
     if use_text_rank_scoring:
-        text_rank_sents_map = extraction_based_summarize_text_rank_sentence_scoring(doc)
+        text_rank_sents_map = \
+                extraction_based_summarize_text_rank_sentence_scoring(doc)
         sent_id_score_maps.append(add_base(text_rank_sents_map, 0.7))
 
     final_map = {}
@@ -109,27 +137,32 @@ def extraction_based_summarize(doc, method, num_sentences):
                 final_map[sent_id] += score
 
     summary_sentences = []
-    sorted_final_map_items = sorted(final_map.items(), key=lambda x: x[1], reverse=True)
+    sorted_final_map_items = sorted(final_map.items(), key=lambda x: x[1],
+                                    reverse=True)
     for sent_id, score in sorted_final_map_items:
         summary_sentences.append((sent_id, doc_sents_list[sent_id]))
     summary_sentences = summary_sentences[:num_sentences]
-    summary_sentences = sorted(summary_sentences, key=lambda x: x[0], reverse=True)
+    summary_sentences = sorted(summary_sentences, key=lambda x: x[0],
+                               reverse=True)
     summary = " ".join([str(sent).strip() for _, sent in summary_sentences])
 
     if use_first_last_scoring:
-        return str(first_sent).strip() + " " + summary + " " + str(last_sent).strip()
+        return str(first_sent).strip() + " " + summary + " " + \
+                    str(last_sent).strip()
     else:
         return summary
 
 
 def abstractive_summarization(text, method):
-    use_t5 = bool((method & ABSTRACT_SUMMARY_T5) > 0)
-    use_pegasus = bool((method & ABSTRACT_SUMMARY_PEGASUS) > 0)
+    use_t5 = is_method_set(method, ABST_T5)
+    use_pegasus = is_method_set(method, ABST_PEGASUS)
 
     if use_pegasus:
-        model = PegasusForConditionalGeneration.from_pretrained("google/pegasus-xsum")
+        model = PegasusForConditionalGeneration.\
+            from_pretrained("google/pegasus-xsum")
         tokenizer = PegasusTokenizer.from_pretrained("google/pegasus-xsum")
-        input_ids = tokenizer.encode(text, return_tensors='pt', max_length=512, truncation=True, padding=True)
+        input_ids = tokenizer.encode(text, return_tensors='pt', max_length=512,
+                                     truncation=True, padding=True)
         summary_ids = model.generate(input_ids, min_length=28, max_length=28)
         return tokenizer.decode(summary_ids[0], skip_special_tokens=True)
 
@@ -139,7 +172,6 @@ def abstractive_summarization(text, method):
         input_ids = tokenizer.encode(text, return_tensors='pt')
         summary_ids = model.generate(input_ids, min_length=30, max_length=120)
         return tokenizer.decode(summary_ids[0], skip_special_tokens=True)
-    
     return "Uknown abstract summary method"
 
 
@@ -148,7 +180,8 @@ def evaluate_summary(generated_summary, reference_summary):
     Ocena jakości podsumowania za pomocą metryk ROUGE i BERTScore.
     """
     # ROUGE
-    rouge = rouge_scorer.RougeScorer(['rouge1', 'rouge2', 'rougeL'], use_stemmer=True)
+    rouge = rouge_scorer.RougeScorer(['rouge1', 'rouge2', 'rougeL'],
+                                     use_stemmer=True)
     rouge_scores = rouge.score(reference_summary, generated_summary)
 
     # BERTScore
@@ -165,45 +198,52 @@ def evaluate_summary(generated_summary, reference_summary):
     return evaluation
 
 
-def hex(value):
-    try:
-        return int(value, 16)
-    except ValueError:
-        raise argparse.ArgumentTypeError(f"Invalid integer value: {value}")
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model', type=str, help="Path to model used for text information extraction using spaCy.")
-    parser.add_argument('--text_path', type=str, help="Path to file containing text to summarize.")
-    parser.add_argument('--method', type=hex, help="Method used to summarize text.\n\
-            \tEXTRACTION_BASED_SUMMARY_NAIVE_SCORING=0x01,\n\
-            \tEXTRACTION_BASED_SUMMARY_FIRST_LAST_SCORING=0x02,\n\
-            \tEXTRACTION_BASED_SUMMARY_TF_IDF_SCORING=0x04,\n\
-            \tEXTRACTION_BASED_SUMMARY_TEXT_RANK_SCORING=0x08,\n\
-            \tABSTRACT_SUMMARY=0x10")
-    parser.add_argument('--num_sentences', type=int, help="Number of sentences in the summary.")
-    parser.add_argument('-rs', '--reference_path', type=str, help="(Optional) Path to file containing reference summary.")
+    parser.add_argument('-m', '--model', type=str, help="""
+            Path to model used for text information extraction using spaCy.
+            """)
+    parser.add_argument('-t', '--text_path', type=str, help="""
+            Path to file containing text to summarize.
+            """)
+    parser.add_argument('-s', '--summary_methods', type=str, help="""
+            Methods used, available
+            options: EXT_NAIVE|EXT_FIRST_LAST|EXT_IF_IDF|EXT_TEXT_RANK or
+            ABST_T5|ABST_PEGASUS. In the case of ABST_ options, order
+            determines in what order methods are applied.
+            """)
+    parser.add_argument('-n', '--num_sentences', type=int, help="""
+            Number of sentences
+            in the summary.
+            """)
+    parser.add_argument('-r', '--reference_path', type=str, help="""
+            (Optional) Path to file containing reference summary.
+            """)
     args = parser.parse_args()
+    methods = parse_method(args.summary_methods)
 
     with open(args.text_path, 'r', encoding='utf-8') as f:
         text = f.read()
     with open(args.reference_path, 'r', encoding='utf-8') as f:
         reference_summary = f.read()
 
-    if (args.method & EXTRACTION_BASED_SUMMARY) > 0:
+    if is_method_set(methods, EXT_SUMMARY):
         nlp = spacy.load(args.model)
         nlp.add_pipe("textrank")
         doc = nlp(text)
-        summary = extraction_based_summarize(doc, (args.method & EXTRACTION_BASED_SUMMARY), args.num_sentences)
+        summary = extraction_based_summarize(doc, is_method_set(methods,
+                                                                EXT_SUMMARY),
+                                             args.num_sentences)
         print(summary)
         if args.reference_path:
-            eval_extraction_based = evaluate_summary(summary, reference_summary)
+            eval_extraction_based = \
+                evaluate_summary(summary, reference_summary)
             print(eval_extraction_based)
 
-    if (args.method & ABSTRACT_SUMMARY) > 0:
-        abstractive_summary = abstractive_summarization(text, (args.method & ABSTRACT_SUMMARY))
+    if is_method_set(methods, ABST_SUMMARY):
+        abstractive_summary = abstractive_summarization(text, methods)
         print(abstractive_summary)
         if args.reference_path:
-            eval_abstractive = evaluate_summary(abstractive_summary, reference_summary)
+            eval_abstractive = evaluate_summary(abstractive_summary,
+                                                reference_summary)
             print(eval_abstractive)
